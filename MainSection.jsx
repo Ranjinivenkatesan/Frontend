a0f52ccae51467049337d37b45c0b777de0ea7e4
Ranjini <121846224+Ranjinivenkatesan@users.noreply.github.com>
2024-10-21 17:13:07 +0530

    Update MainSection.jsx

--- MainSection.jsx
+++ MainSection.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 const MainSection = () => {
     return (
-        <div>MainSection</div>
-    )
+       <main className='' style={{margin:'auto',height:'auto'}}>
+       <div className='row container' style={{marginTop:'7rem',marginLeft:'auto'}}>
+        <div className='  col-6'>
+            <img className='image' src="https://img.freepik.com/free-photo/3d-cartoon-style-character_23-2151033983.jpg?size=626&ext=jpg&uid=R155751979&ga=GA1.1.499466454.1729482530&semt=ais_hybrid" alt="Hel" width={'300px'} />
+        </div>
+        <div className='content d-flex flex-column justify-content-start col-6'>
+            <h2>Ranjini V</h2>
+            <hr />
+            <h4>Full Stack Developer</h4>
+            <h5>BE Computer Science and Engineering</h5>
+            <p>Contact:+916385434507</p>
+            <p>Email: ranjivenkatasan@gmail.com</p>
+        </div>
+        </div> 
+
+    </main>
+
+  )
 }
 
 export default MainSection
